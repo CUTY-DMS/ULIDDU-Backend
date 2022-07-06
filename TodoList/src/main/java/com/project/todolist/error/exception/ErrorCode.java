805eccf4e9836b2414afe87060d6104c830e0c05
rlaisqls <rlaisqls@gmail.com>
2022-07-06 10:29:24 +0900

    🐛 :: 에러처리

--- TodoList/src/main/java/com/project/todolist/error/exception/ErrorCode.java
+++ TodoList/src/main/java/com/project/todolist/error/exception/ErrorCode.java
@@ -10,23 +10,23 @@ import lombok.Getter;
 public enum ErrorCode {
 
     //요청 형식 오류
-    INVALID_PARAMETER(400, "Invalid Parameter"),
-    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INVALID_PARAMETER(400, "요청정보가 잘못됐습니다"),
+    METHOD_NOT_ALLOWED(405, "해당 메서드를 찾을 수 없습니다"),
 
     //토큰
-    TOKEN_EXPIRED(401 , "Expired Token"),
-    TOKEN_INVALID(401, "Invalid Token"),
+    TOKEN_EXPIRED(401 , "만료된 토큰입니다"),
+    TOKEN_INVALID(401, "인증되지 않은 토큰입니다"),
 
     //유저
-    USER_NOT_FOUND(404,"User Nor Found"),
-    USER_ALREADY_EXISTS(409, "User Already Exists"),
-    FORBIDDEN(403, "Forbidden"),
+    USER_NOT_FOUND(404,"유저를 찾을 수 없습니다"),
+    USER_ALREADY_EXISTS(409, "이미 존재하는 유저입니다"),
+    FORBIDDEN(403, "권한이 없습니다"),
 
     //투두
-    TODO_NOT_FOUND(404, "Todo Not Found"),
+    TODO_NOT_FOUND(404, "해당 투두를 찾을 수 없습니다"),
 
     //서버문제
-    INTERNAL_SERVER_ERROR(500,"Server Error");
+    INTERNAL_SERVER_ERROR(500,"서버 에러");
 
     private final int statusCode;
     private final String message;
