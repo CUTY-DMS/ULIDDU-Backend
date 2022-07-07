dbdaa6047f59156e2ba4d9ea6ba58339184ec0ea
rlaisqls <rlaisqls@gmail.com>
2022-07-07 17:14:24 +0900

    ♻️ :: CrudRepository로 변경

--- TodoList/src/main/java/com/project/todolist/entity/user/UserRepository.java
+++ TodoList/src/main/java/com/project/todolist/entity/user/UserRepository.java
@@ -1,11 +1,12 @@
 package com.project.todolist.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findById(Long id);
 
     Optional<User> findByUserId(String userId);
